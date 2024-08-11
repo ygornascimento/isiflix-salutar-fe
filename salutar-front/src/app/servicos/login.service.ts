ff8b02bcf9eb27de82279b18b28ea3c42d28e404
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-08-11 12:28:36 -0300

    Added environments

--- salutar-front/src/app/servicos/login.service.ts
+++ salutar-front/src/app/servicos/login.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Usuario } from '../model/Usuario';
+import { Observable } from 'rxjs';
+import { SalutarToken } from '../model/SalutarToken';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +12,7 @@ export class LoginService {
 
   constructor(private http: HttpClient) {}
 
-  public efetuarLogin(): any {
-    
+  public efetuarLogin(usuario: Usuario): Observable<SalutarToken> {
+    return this.http.post<SalutarToken>(environment.apiURL+"/login", usuario);
   }
 }
