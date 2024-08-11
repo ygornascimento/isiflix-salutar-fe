598b580e5820710b4e9771dab490f67d0c1bb11a
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-08-11 17:26:54 -0300

    Testing login with back-end

---
+++ salutar-front/src/app/componentes/loding/loding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loding',
+  standalone: true,
+  imports: [],
+  templateUrl: './loding.component.html',
+  styleUrl: './loding.component.css'
+})
+export class LodingComponent {
+
+}
--- salutar-front/src/app/componentes/login/login.component.ts
+++ salutar-front/src/app/componentes/login/login.component.ts
@@ -1,22 +1,45 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MainComponent } from '../main/main.component';
+import { LoginService } from '../../servicos/login.service';
+import { Usuario } from '../../model/Usuario';
+import { FormsModule } from '@angular/forms';
+import { SalutarToken } from '../../model/SalutarToken';
+import { LodingComponent } from "../loding/loding.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, LodingComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  public constructor(private route:Router) {
+  public usuario: Usuario = new Usuario()
+  public loading: boolean = false;
+
+  public constructor(private route: Router, private service: LoginService) {
 
   }
 
   public logar() {
-    this.route.navigate(['main']);
-  }
+    this.loading = true;
+    this.service
+      .efetuarLogin(this.usuario)
+      .subscribe(
+          {
+            next: (res: SalutarToken) => {
+              this.loading = false;
+              alert("Login deu certo!!!")
+              // localStorage.setItem("SalutarTK", res.token)
+            },
 
+            error: (err: any) => {
+              alert("Login falhou!");
+              this.loading = false;
+            }
+      }
+    );
+  }
 }
