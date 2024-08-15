90cc440dadf929df66c84b7985c331b010e931dc
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-08-14 21:20:19 -0300

    Solve problems with folders

---
+++ salutar/src/app/components/fichapaciente/fichapaciente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fichapaciente',
+  standalone: true,
+  imports: [],
+  templateUrl: './fichapaciente.component.html',
+  styleUrl: './fichapaciente.component.css'
+})
+export class FichapacienteComponent {
+
+}
---
+++ salutar/src/app/model/SalutarToken.ts
@@ -0,0 +1,3 @@
+export class SalutarToken {
+    token: string = "";
+}
